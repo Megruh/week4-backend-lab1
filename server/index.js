85c85fca76518c6c5d897f25de7d6a6ce78b805c
Meg McCauley <memi.mc14@gmail.com>
2022-02-07 14:27:27 -0700

    finished week 4 back end lab

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/api/users", (req, res) => {
+  let friends = ["Nitin", "Eric", "Jeddy", "Cameron", "Riley"];
+  res.status(200).send(friends);
+});
+
+app.get("/weather/:temperature", (req, res) => {
+    console.log(req.query)
+  const phrase = `<h3>It was ${req.params.temperature} today</h3>`;
+  res.status(200).send(phrase);
+});
+
+const SERVER_PORT = 4000;
+
+app.listen(SERVER_PORT, () => {
+  console.log(`listening on port ${SERVER_PORT}`);
+});
